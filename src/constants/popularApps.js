e09bdf0a0b53fd82b0fcb455f409dc983b866fa5
bob <bob@projectbabbage.com>
2023-10-14 16:44:02 +0100

    WIP initial files - not been able to run without MetaNet Client

---
+++ src/constants/popularApps.js
@@ -0,0 +1,6 @@
+const POPULAR_APPS = [
+  'botcrafter.io',
+  'peermail.babbage.systems',
+  'todo.babbage.systems',
+  'bitgenius.net',
+  'peerpay.babbage.systems']
--- src/pages/Dashboard/Actions/index.jsx
+++ src/pages/Dashboard/Actions/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Typography, Grid } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import style from './style'
-import ActionList from '../../../components/ActionList/index.jsx'
 import { useBreakpoint } from '../../../utils/useBreakpoints'
 import Profile from '../../../components/Profile'
+import MetaNetApp from '../../../components/MetaNetApp'
+import POPULAR_APPS from '../../../constants/popularApps'
 
 const useStyles = makeStyles(style, {
   name: 'Actions'
@@ -12,23 +13,84 @@ const useStyles = makeStyles(style, {
 const Actions = ({ history }) => {
   const classes = useStyles()
   const breakpoints = useBreakpoint()
+  const [apps, setApps] = useState([])
+  const [recentApps, setRecentApps] = useState([])
 
+  useEffect(async () => {
+
+    // Obtain a list of all apps ordered chronologically
+    try {
+      const results = await window.CWI.ninja.getTransactionLabels({
+        prefix: 'babbage_app_',
+        sortBy: 'date'
+      })
+      if (results && Array.isArray(results.labels)) {
+        setRecentApps(results.labels)
+      }
+    } catch (error) {
+      console.error('ERROR: Obtaining recent apps in date order:', error)
+    }
+  
+    // Obtain a list of all apps ordered alphabetically
+    try {
+      const results = await window.CWI.ninja.getTransactionLabels({
+        prefix: 'babbage_app_',
+        sortBy: 'label'
+      })
+      if (results && Array.isArray(results.labels)) {
+        setApps(results.labels)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
   return (
     <>
       {(!breakpoints.sm && !breakpoints.xs)
         ? (
           <div className={classes.fixed_nav}>
-            <Typography variant='h1' paragraph>
-              Actions
+            {(recentApps.length <= 5) 
+              ? (
+                <><Typography variant='h3' gutterBottom style={{ paddingBottom: '0.2em' }}>
+                    Popular Apps
+                  </Typography><Grid container spacing={2}>
+                    {POPULAR_APPS.map((name, index) => (
+                      <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                        <MetaNetApp domain={name} />
+                      </Grid>
+                    ))}
+                  </Grid></>
+                )
+              : (
+                <><Typography variant='h3' gutterBottom style={{ paddingBottom: '0.2em' }}>
+                    Recent Apps
+                  </Typography><Grid container spacing={2}>
+                    {recentApps.map((app, index) => (
+                      <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                        <MetaNetApp domain={app.label.replace(/^babbage_app_/, '')} />
+                      </Grid>
+                    ))}
+                  </Grid></>
+                )
+            }
+            <Typography variant='h3' gutterBottom style={{ paddingBottom: '0.2em' }}>
+              All Apps
             </Typography>
+            <Grid container spacing={2}>
+              {apps.map((app, index) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                  <MetaNetApp domain={app.label.replace(/^babbage_app_/, '')} />
+                </Grid>
+              ))}
+            </Grid>
           </div>
           )
         : (
           <Profile />
           )}
-      <ActionList />
     </>
   )
 }
-
 export default Actions
+
+
