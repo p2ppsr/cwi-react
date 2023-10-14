78e6ea38476f4a62c0576dfdc8dc289d966b5e00
Brayden Langley <brayden167@gmail.com>
2023-10-13 18:42:26 -0700

    Added an all apps section (#40)
    * Added an all apps section
    
    * Use getTransactionLabels
    
    * Bug fixes
    
    * Bug fixes

--- src/pages/Dashboard/Actions/index.jsx
+++ src/pages/Dashboard/Actions/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Typography, Grid } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import style from './style'
-import ActionList from '../../../components/ActionList/index.jsx'
 import { useBreakpoint } from '../../../utils/useBreakpoints'
 import Profile from '../../../components/Profile'
+import MetaNetApp from '../../../components/MetaNetApp'
 
 const useStyles = makeStyles(style, {
   name: 'Actions'
@@ -12,21 +12,42 @@ const useStyles = makeStyles(style, {
 const Actions = ({ history }) => {
   const classes = useStyles()
   const breakpoints = useBreakpoint()
+  const [apps, setApps] = useState([])
 
+  useEffect(async () => {
+    // Obtain a list of all apps ordered alphabetically
+    try {
+      const results = await window.CWI.ninja.getTransactionLabels({
+        prefix: 'babbage_app_',
+        sortBy: 'label'
+      })
+      if (results && Array.isArray(results.labels)) {
+        setApps(results.labels)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
   return (
     <>
       {(!breakpoints.sm && !breakpoints.xs)
         ? (
           <div className={classes.fixed_nav}>
-            <Typography variant='h1' paragraph>
-              Actions
+            <Typography variant='h3' gutterBottom style={{ paddingBottom: '0.2em' }}>
+              All Apps
             </Typography>
+            <Grid container spacing={2}>
+              {apps.map((app, index) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                  <MetaNetApp domain={app.label.replace(/^babbage_app_/, '')} />
+                </Grid>
+              ))}
+            </Grid>
           </div>
           )
         : (
           <Profile />
           )}
-      <ActionList />
     </>
   )
 }
