b8b3317ff0b08eae5f6c64ebf45f8ec76c3282e5
Brayden Langley <brayden167@gmail.com>
2023-10-17 12:27:57 -0700

    Merged Bob and Brayden's changes

--- src/constants/popularApps.js
+++ src/constants/popularApps.js
@@ -1,6 +1,22 @@
-const POPULAR_APPS = [
-  'botcrafter.io',
-  'peermail.babbage.systems',
-  'todo.babbage.systems',
-  'bitgenius.net',
-  'peerpay.babbage.systems']
+export default [
+  {
+    appName: 'BotCrafter',
+    appIconImageUrl: 'https://botcrafter.io/favicon.ico',
+    domain: 'botcrafter.io'
+  },
+  {
+    appName: 'TODO',
+    appIconImageUrl: 'https://todo.babbage.systems/favicon.ico',
+    domain: 'todo.babbage.systems'
+  },
+  {
+    appName: 'BitGenius',
+    appIconImageUrl: 'https://bitgenius.net/favicon.ico',
+    domain: 'bitgenius.net'
+  },
+  {
+    appName: 'PeerPay',
+    appIconImageUrl: 'https://projectbabbage.com/favicon.ico',
+    domain: 'peerpay.babbage.systems'
+  }
+]
