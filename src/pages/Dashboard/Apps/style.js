8d6bdf27ab14a319ebc917194c2e21c928208b34
Brayden Langley <brayden167@gmail.com>
2023-11-06 15:55:44 -0800

    Bug fix

--- src/pages/Dashboard/Apps/style.js
+++ src/pages/Dashboard/Apps/style.js
@@ -3,17 +3,21 @@ export default theme => ({
     padding: '1em 1em 2em 1em'
   },
   gridItem: {
-    minWidth: '16em',
     height: '14em',
+    minWidth: '10em',
 
-    '@media (max-width: 1200px) and (min-width: 999px)': {
+    '@media (max-width: 1400px) and (min-width: 1201px)': {
       height: '10em',
       maxWidth: '16em',
       minWidth: '10em'
     },
-    '@media (max-width: 998px) and (min-width: 500px)': {
+    '@media (max-width: 1200px) and (min-width: 900px)': {
       height: '10em',
-      minWidth: '4em'
+      minWidth: '8em'
+    },
+    '@media (max-width: 899px) and (min-width: 500px)': {
+      height: '10em',
+      minWidth: '8em'
     },
     '@media (max-width: 499px) and (min-width: 0)': {
       height: '8em',
