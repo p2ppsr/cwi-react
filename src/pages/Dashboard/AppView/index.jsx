414f6cfca9cecb306a2bfe48d96ea8a2ccf50377
bob <bob@projectbabbage.com>
2023-11-27 21:56:19 +0000

    tabbing functionality improved needs history  WIP

--- src/pages/Dashboard/AppView/index.jsx
+++ src/pages/Dashboard/AppView/index.jsx
@@ -18,12 +18,12 @@ const useStyles = makeStyles(style, { name: 'appview' })
 
 const AppView = ({ match, history }) => {
   console.log('AppView:match=', match)
-  const appDomain = decodeURIComponent(match.params.originator)
-  console.log('AppView:appDomain=', appDomain)
-  const [appName, setAppName] = useState(appDomain)
+  const originator = decodeURIComponent(match.params.originator)
+  console.log('AppView:originator=', originator)
+  // const [isInit, setIsInit] = useState(true)
+  const [appDomain, setAppDomain] = useState('')
+  const [appName, setAppName] = useState('')
   const [appIcon, setAppIcon] = useState('MetaNet AppMetaNet App')
-  // const [displayLimit, setDisplayLimit] = useState(5)
-  // const [appActions, setAppActions] = useState({})
   const [loading, setLoading] = useState(false)
   const [refresh, setRefresh] = useState(false)
 
@@ -34,48 +34,51 @@ const AppView = ({ match, history }) => {
     // setDisplayLimit,
     setRefresh
   }
-
   const classes = useStyles()
-  // alert('AppView')
   useEffect(() => {
     (async () => {
       try {
+        if (appDomain === '') {
+          setAppDomain(originator)
+          console.log('AppView:appDomain=', appDomain)
+        }
         setLoading(true)
         // Validate that the default favicon path is actually an image
-        if (await isImageUrl(`https://${appDomain}/favicon.ico`)) {
-          setAppIcon(`https://${appDomain}/favicon.ico`)
-        } else {
-          setAppIcon('https://projectbabbage.com/favicon.ico')
+        try {
+          const manifest = await parseAppManifest({ domain: appDomain })
+          if (manifest) {
+            if (await isImageUrl(`https://${appDomain}/favicon.ico`)) {
+              setAppIcon(`https://${appDomain}/favicon.ico`)
+            } else {
+              setAppIcon('https://projectbabbage.com/favicon.ico')
+            }
+            // Try to parse the app manifest to find the app info
+            try {
+              const manifest = await parseAppManifest({ domain: appDomain })
+              console.log('AppView:manifest=', manifest)
+              console.log('AppView:typeof manifest.name=', typeof manifest.name)
+              if (typeof manifest.name === 'string') {
+                setAppName(manifest.name)
+              }
+            } catch (error) {
+              console.error(error)
+            }
+            setLoading(false)
+            setRefresh(false)
+            console.log('reloaded')
+          }
+        } catch (e) {
         }
-        // Try to parse the app manifest to find the app info
-        const manifest = await parseAppManifest({ domain: appDomain })
-        if (typeof manifest.name === 'string') {
-          setAppName(manifest.name)
-        }
-
-        // Get a list of the 5 most recent actions from the app
-        // Also request input and output amounts and descriptions from Ninja
-        const appActions = await window.CWI.ninja.getTransactions({
-          // limit: displayLimit,
-          includeBasket: true,
-          includeTags: true,
-          order: 'descending',
-          label: `babbage_app_${appDomain}`,
-          addInputsAndOutputs: true,
-          status: 'completed'
-        })
-        console.log(appActions)
-        // setAppActions(appActions)
-        setLoading(false)
-        setRefresh(false)
-        console.log('reloaded')
       } catch (e) {
         /* do nothing */
         setLoading(false)
         setRefresh(false)
       }
     })()
-  }, [refresh])
+  }, [refresh, appDomain])
+  // }, [refresh, appDomain, appName, isInit, loading, appIcon])
+  const isInit = originator === appDomain || appDomain === ''
+  console.log('AppView:isInit=', isInit)
 
   // * className={classes.page_container}
   return (
@@ -114,6 +117,7 @@ const AppView = ({ match, history }) => {
             </Typography>
             <br />
             <AppViewTabs />
+            {isInit && <ProtocolList />}
           </div>
           <div>
             <Button
@@ -131,6 +135,7 @@ const AppView = ({ match, history }) => {
         </div>
       </div>
       <div>
+
       <Switch>
         <Route
           path='/dashboard/manage-app/protocols'
